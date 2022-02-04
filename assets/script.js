17ec23b543d58745318decdf5a89292f2fa28e77
smarr2198 <smarr2198@gmail.com>
2022-02-03 22:01:36 -0600

    set search history to local storage

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 const cityArray = JSON.parse(localStorage.getItem("cityHistory")) || [];
 // functions
 function handleCoords(searchCity) {
-  const fetchUrl = `http://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=4b9f7dc3f8536150bc0eb915e8e4a81b`;
+  const fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=4b9f7dc3f8536150bc0eb915e8e4a81b`;
 
   fetch(fetchUrl)
     .then(function (response) {
